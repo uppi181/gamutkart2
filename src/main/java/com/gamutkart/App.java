93063b1e8cd41aee23e6bc2c9465252d1afaf4f2
Ubuntu <ubuntu@ip-172-31-41-86.ec2.internal>
2024-02-05 14:26:36 +0000

    adding all files from nagesh

---
+++ src/main/java/com/gamutkart/App.java
@@ -0,0 +1,17 @@
+package com.gamutkart;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+		int k;
+		int j;
+		int l;
+
+		for(i=0;i<=30;i++)
+		{
+			i += 5;
+   		}
+	 }
+}
